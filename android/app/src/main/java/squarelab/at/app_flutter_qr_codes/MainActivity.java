09e719527b0ea70682fea8219638e48978496517
Christoph Wolf <wolf.christoph@gmx.at>
2019-08-02 18:54:32 +0200

    Initial Commit

---
+++ android/app/src/main/java/squarelab/at/app_flutter_qr_codes/MainActivity.java
@@ -0,0 +1,13 @@
+package squarelab.at.app_flutter_qr_codes;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
